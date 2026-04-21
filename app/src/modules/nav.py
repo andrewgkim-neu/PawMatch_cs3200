90161d693dd2b0c0990b3db721425c7f3fdfdacb
Sophia Noelle Romualdez <sophianoelleromualdez@Sophias-MacBook-Air.local>
2026-04-20 21:51:19 -0400

    logo

--- app/src/modules/nav.py
+++ app/src/modules/nav.py
@@ -120,7 +120,7 @@ def SideBarLinks(show_home=False):
     """
 
     # Logo appears at the top of the sidebar on every page
-    st.sidebar.image("assets/logo.png", width=150)
+    st.sidebar.image("assets/pawmatch_logo_v5.png", width=150)
 
     # If no one is logged in, send them to the Home (login) page
     if "authenticated" not in st.session_state:
